c6c4e935a044ef46c22873348a39fbce209ce565
Dimitris Greasidis <dgreasidis+github@gmail.com>
2022-06-17 10:30:59 +0300

    feat: add Dropzone component
    user can open or drag the file into the page and the file will be loaded in the page

--- src/features/Home/HomeScreen.tsx
+++ src/features/Home/HomeScreen.tsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Dropzone from './components/Dropzone';
 
 const HomeScreen = () => {
+  const [fileString, setFile] = useState<string>('');
   return (
     <div className="padding-10">
       <div className="flex-center-start-column-nowrap">
+        <Dropzone setFile={setFile} />
       </div>
     </div>
   );
---
+++ src/features/Home/components/Dropzone.tsx
@@ -0,0 +1,32 @@
+import React, { useCallback } from 'react';
+import { DropEvent, FileRejection, useDropzone } from 'react-dropzone';
+import { useFileReader } from '../../../hooks';
+
+interface Props {
+  setFile: (file: string) => void;
+}
+
+type T = File;
+
+const Dropzone = ({ setFile }: Props) => {
+  const setFilePath = useFileReader(setFile);
+
+  const onDrop = useCallback((acceptedFiles: T[], fileRejections: FileRejection[], event: DropEvent) => {
+    if (acceptedFiles[0]) setFilePath(acceptedFiles[0]);
+  }, []);
+
+  const { getRootProps, getInputProps } = useDropzone({
+    accept: { 'text/xml': ['.xml'] },
+    multiple: false,
+    onDrop,
+  });
+
+  return (
+    <div {...getRootProps({ className: 'dropzone' })} className="dropzone-container padding-10">
+      <input {...getInputProps()} />
+      <div className="font18">Drop or click to upload</div>
+    </div>
+  );
+};
+
+export default Dropzone;
